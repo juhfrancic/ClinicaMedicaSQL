8b3adcfb833389b0b50f9c523bf6fb9ebc0f0451
juhfrancic <juliafrancictostes@gmail.com>
2025-11-05 23:17:00 -0300

    Adicionando alguns insert into

--- ClinicaMedicaSQL.sql
+++ ClinicaMedicaSQL.sql
@@ -159,3 +159,36 @@ ADD FOREIGN KEY (idMedico) REFERENCES Medicos(idMedico)
 ALTER TABLE MedicosEspecialidades
 ADD FOREIGN KEY (idMedico) REFERENCES Medicos(idMedico),
 FOREIGN KEY (idEspecialidade) REFERENCES EspecialidadesMedicas(idEspecialidade)
+
+
+SELECT * FROM TiposPlanos 
+
+INSERT INTO TiposPlanos (TipoPlano) VALUES('BASICO')
+INSERT INTO TiposPlanos VALUES('PREMIUM')
+INSERT INTO TiposPlanos VALUES('MASTER'),('PLUS ADVANCED')
+
+INSERT INTO TiposConsultas VALUES ('Primeira Consulta', 200.50),
+                                 ('Retorno', 720),
+                                 ('Emergencia', 1200)
+                                 
+SELECT *FROM TiposConsultas
+
+INSERT INTO StatusConsultas VALUES ('Agendada'), ('Cancelada'), ('Realizada')
+
+SELECT * FROM StatusConsultas 
+
+UPDATE StatusConsultas 
+SET StatusConsulta = 'Concluida'
+WHERE StatusConsulta = 'Realizada'
+
+ALTER TABLE EspecialidadesMedicas
+ALTER COLUMN NomeEspecialidade VARCHAR(50) NOT NULL
+
+INSERT INTO EspecialidadesMedicas VALUES 
+('Pediatra', 'Cuida das Crianças'),
+('Cardiologista', 'Cuda do Coração'),
+('Otorrinolaringologista', 'Esse fala por si só'),
+('Dermatologista', ''),
+('Urologista', 'Cuida dos homens');
+
+SELECT * FROM EspecialidadesMedicas
